51abfbbeca0d829e963130bb013f8616e500f70a
umairjavaid <bscs13024@itu.edu.pk>
2020-08-17 04:57:10 +0500

    Update __init__.py

--- wsol/method/__init__.py
+++ wsol/method/__init__.py
@@ -2,5 +2,6 @@ from .has import has
 from .acol import AcolBase
 from .adl import ADL
 from .cutmix import cutmix
+from .mymodel2 import MyModel2
 from .util import normalize_tensor
 from .util import get_attention
